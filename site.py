3f8b095e9a29aae67126df329dc1d94e1648cbc5
David Terry <davidterry@posteo.de>
2016-07-12 19:40:32 +0100

    Add login error

--- site.py
+++ site.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def login():
     return render_template("login.html")
 
+@app.route("/login_error")
+def login_error():
+    return render_template("login.html", error=True)
+
 @app.route("/logged_in")
 def logged_in():
     return render_template("logged_in.html")
